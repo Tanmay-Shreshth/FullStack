5f9f6edc24273047e3e3cb3dd3e2d468ac81920a
TANMAYSHRESHTH <tanmay.1822cs1151@kiet.edu>
2024-09-16 03:00:35 +0530

    employee table created

---
+++ ang-front/src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
+@Component({
+  selector: 'app-employee-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './employee-list.component.html',
+  styleUrl: './employee-list.component.css'
+})
+export class EmployeeListComponent implements OnInit{
+  
+  employees: Employee[];
+
+  ngOnInit(): void {
+    this.employees = [
+      {
+        "id": 1,
+        "firstName": "Tanmay",
+        "lastName": "Shreshth",
+        "emailId": "tanmay@gmail.com"
+      },
+      {
+        "id": 2,
+        "firstName": "Anyone",
+        "lastName": "Anonym",
+        "emailId": "anyone@anyone.com"
+      }
+    ];
+  }
+
+}
