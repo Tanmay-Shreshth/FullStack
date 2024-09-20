56a556b073e17be317717ded6337b090e3157976
Tanmay-Shreshth <tanmayshreshth004@gmail.com>
2024-09-21 02:13:21 +0530

    temporary changes

--- ang-front/src/app/employee-list/employee-list.component.ts
+++ ang-front/src/app/employee-list/employee-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -12,23 +13,16 @@ export class EmployeeListComponent implements OnInit{
   
   employees: Employee[];
 
-  constructor(){};
+  constructor(private employeeService : EmployeeService){};
 
   ngOnInit(): void {
-    this.employees = [
-      {
-        "id": 1,
-        "firstName": "Tanmay",
-        "lastName": "Shreshth",
-        "emailId": "tanmay@gmail.com"
-      },
-      {
-        "id": 2,
-        "firstName": "Anyone",
-        "lastName": "Anonym",
-        "emailId": "anyone@anyone.com"
-      }
-    ];
+    this.getEmployeeList();
+  }
+
+  private getEmployeeList(){
+    this.employeeService.getEmployeesList().subscribe(employee=> {
+      this.employees = employee;
+    });
   }
 
 }
