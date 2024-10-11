366c16ce1f234040a4b61f4bcf9139405c141758
Tanmay-Shreshth <tanmayshreshth004@gmail.com>
2024-10-11 21:25:10 +0530

    added cross origin

--- fullstack/src/main/java/com/personal/fullstack/controller/EmployeeController.java
+++ fullstack/src/main/java/com/personal/fullstack/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.personal.fullstack.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.personal.fullstack.services.employee.EmployeeServices;
 
 @RestController
 @RequestMapping("/employee")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EmployeeController {
     
     @Autowired
